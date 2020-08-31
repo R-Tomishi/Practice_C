6b4fc0770a4f120bb32d1770b7a7a6c1fbb0006a
Ryota Tomishi <>
2020-08-31 18:42:52 +0900

    関数練習

--- test.c
+++ test.c
@@ -5,44 +5,16 @@
 
 typedef char String[1024];
 
+void hello(void)
+{
+	printf("こんにちは\n");
+}
+
 int main(void)
 {
-	srand((unsigned)time(0UL));
-	printf("***数あてゲーム(レベル2)***\n");
-	printf("3桁の数を当ててください!\n");
-	int answer[3] = {rand() % 10, rand() % 10, rand() % 10};
-	int input[3] = {};
-	String In_n;
+	printf("関数を呼び出します\n");
+	hello();
+	printf("関数の呼び出しが終わりました\n");
 
-	String game_s;
-	int game = 1;
-	while (game == 1) {
-		for (int i = 0; i < 3; i++) {
-			printf("%d桁目の予想を0〜9の数字で入力してください：\n", i+1);
-			scanf("%s", In_n);
-			input[i] = atoi(In_n);
-		}
-		int hit = 0;
-		int bro = 0;
-		for (int i = 0; i < 3; i++) {
-			if (input[i] == answer[i]) {
-				hit += 1;
-			}
-			for (int j = 0; j < 3; j++) {
-				if (input[i] == input[j] && i !=j) {
-					bro +=1;
-				}
-			}
-		}
-		printf("%dヒット、%dブロー\n", hit, bro);
-		if (hit == 3) {
-			printf("%d%d%d正解です!!\n", answer[0], answer[1], answer[2]);
-			break;
-		}
-		printf("続けますか?　（1:続ける 0:終了）:\n");
-		scanf("%s", game_s);
-		game = atoi(game_s);
-	}
-	printf("ゲーム終了。正解は%d%d%dでした\n", answer[0], answer[1], answer[2]);
 	return 0;
 }
