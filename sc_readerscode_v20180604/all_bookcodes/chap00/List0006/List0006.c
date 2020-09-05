19d3894331560d5314408e5ee2b4053a5d59724a
Ryota Tomishi <>
2020-09-05 23:09:56 +0900

    教科書の回答例を追加

---
+++ sc_readerscode_v20180604/all_bookcodes/chap00/List0006/List0006.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("かいとう\n");
+  printf("31歳\n");
+  printf("アンティークが好きです\n");
+  printf("31 + 31の計算をします\n");
+  printf("%d\n", 31 + 31);
+  int x;
+  x = 6;
+  printf("%d\n", x * x * 3);
+  return 0;
+}
